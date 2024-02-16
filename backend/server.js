ba50806c68aac710d82fd963d11e32dd2ae759ba
humiba <huaminhbao7a4@gmail.com>
2024-02-16 11:47:22 +0700

    config for deployment -  wait for deploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path')
 
 const appRoute = require('./routes');
 const { connectToMongoDB } = require('./db/connectToMongoDB');
@@ -14,6 +15,12 @@ app.use(express.json()); // Parse JSON bodies from incoming requests
 app.use(cookieParser());
 app.use('/api', appRoute); // Routers
 app.use(cors({ credentials: true }));
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
